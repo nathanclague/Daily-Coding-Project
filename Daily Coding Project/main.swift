f1bea7aef05c4fc87f7c2fa28da023f611402f2b
Nathan Clague <nathanclague@gmail.com>
2022-03-19 14:36:52 -0500

    Problem 2

--- Daily Coding Project/main.swift
+++ Daily Coding Project/main.swift
@@ -30,3 +30,26 @@ where T.Element == U {
 //  Without further clarification of what constitutes a "pass", it is difficult to answer this question.
 //  If "pass" is relative to O() performance, the best option that I could come up with would be O(n log n). My process would begin with sorting the sequence / list. Then, I would take the 1st element in the sequence / list and search for its compliment in the sorted sequence / list using a binary search.
 //  If "pass" is defined as touching, accessing, or reading each element in the sequence / list exactly once, then the only way to aproach this problem would be to require that the input sequence / list be pre-sorted. Assuming that the sequence is sorted in ascending order, you could start by accessing the elements at the head and tail of the sequence. If their sum is greater than k, compare the head with the second to last. And if the sum is less than k, compare the second item with the tail. Repeat these steps until your indicies cross or until you reach a pair that add up to k. One caveat to this approach is that you technically end up accessing each element in the array twice on average.
+
+
+
+//  Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+//
+//For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+//
+//Follow-up: what if you can't use division?
+
+func problem_2(arr: [Int]) -> [Int] {
+    var product = 1
+    var output: [Int] = []
+    for value in arr {
+        product = product * value
+    }
+    for value in arr {
+        output.append(product / value)
+    }
+    return output
+}
+
+print(problem_2(arr: [1, 2, 3, 4, 5]))
+print(problem_2(arr: [3, 2, 1]))
